26984bea4d88e26f41f53b7e1a2699f40b930d87
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 21:01:08 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,8 +2,25 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-  def city_openings(date1, date2)
-      self.listings.joins(host: :reservations).where.not('reservations.checkin >= ? and reservations.checkout <= ?', date1, date2)
+  def city_openings(datein, dateout)
+    #takes a date range
+    #finds listing within that range
+    #outputs those listing
+    binding.pry
+    openings = {}
+    self.listings.each do |listings|
+      listings.reservations.each do |reservation|
+        checkin = reservation.checkin
+        checkout = reservation.checkout
+        #binding.pry
+        if [datein.to_date, checkin].max > [dateout.to_date, checkout].min
+          res
+        end
+      end
+  
+      openings
+  
+    end
   end
 
   def self.highest_ratio_res_to_listings
