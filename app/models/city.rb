9307ecd3dea927abfa2e595867e8cad69dd6b15e
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 18:35:55 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,8 +3,13 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
 
   def city_openings(date1, date2)
+    #takes a date range
+    #finds listing within that range
+    #outputs those listing
+    self.listings.select do |listing|
+      listing.between?(date1, date2)
+    end
     
-
   end
 
 end
