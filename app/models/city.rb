c83075f9d802c9c80b65acc4fbd78849a573a8ce
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 19:30:57 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,35 +2,28 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-  def city_openings(datein, dateout)
-    #takes a date range
-    #finds listing within that range
-    #outputs those listing
-    openings = []
-    self.listings.each do |listings|
-      listings.reservations.each do |reservation|
-        checkin = reservation.checkin
-        checkout = reservation.checkout
-        binding.pry
-        if [datein.to_date, checkin].max < [dateout.to_date, checkout].min
-          openings << listings
-        end
-      end
+  # def city_openings(datein, dateout)
+  #   #takes a date range
+  #   #finds listing within that range
+  #   #outputs those listing
+  #   openings = []
+  #   self.listings.each do |listings|
+  #     listings.reservations.each do |reservation|
+  #       checkin = reservation.checkin
+  #       checkout = reservation.checkout
+  #       binding.pry
+  #       if [datein.to_date, checkin].max > [dateout.to_date, checkout].min
+  #         openings << listings
+  #       end
+  #     end
+  #
+  #     openings
+  #
+  #   end
+  #
+  #
+  # end
 
-      openings
-
-    end
-
-
-  end
-    #     checkin = listing.checkin
-    #     checkout = listing.checkout
-    #     if max(datein, checkin) < min(dateout, checkout)
-    #       openings << listing
-    #     end
-    #   end
-    # end
-
-
+  
 
 end
