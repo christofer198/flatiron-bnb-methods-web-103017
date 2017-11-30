62c3fabaf061204de933534529e75eee0efe544f
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 21:10:34 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -7,19 +7,19 @@ class City < ActiveRecord::Base
     #finds listing within that range
     #outputs those listing
     binding.pry
-    # openings = Hash.new(0)
-    # self.listings.each do |listing|
-    #   listing.reservations.each do |reservation|
-    #     binding.pry
-    #     checkin = reservation.checkin
-    #     checkout = reservation.checkout
-    #
-    #     #binding.pry
-    #     if [datein.to_date, checkin].max > [dateout.to_date, checkout].min
-    #       openings[listing] += 1
-    #     end
-    #   end
-    # end
+    openings = Hash.new(0)
+    self.listings.each do |listing|
+      listing.reservations.each do |reservation|
+        binding.pry
+        checkin = reservation.checkin
+        checkout = reservation.checkout
+
+        #binding.pry
+        if [datein.to_date, checkin].max > [dateout.to_date, checkout].min
+          openings[listing] += 1
+        end
+      end
+    end
   end
 
   def self.highest_ratio_res_to_listings
