7688a6f8cd8b9d69361d12b5b218e3021c8c1c62
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 18:50:05 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -8,8 +8,9 @@ class City < ActiveRecord::Base
     #outputs those listing
     openings = []
     self.listings.each do |listings|
-      byebug
-      puts listings
+      listings.each do |x|
+        puts x
+      end
     end
     #     checkin = listing.checkin
     #     checkout = listing.checkout
