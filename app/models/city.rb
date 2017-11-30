f0a6b187525f2de4c07d2bb2abf5148c8a927193
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 18:29:00 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,7 +2,8 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-  def city_openings
+  def city_openings(date1, date2)
+    
 
   end
 
