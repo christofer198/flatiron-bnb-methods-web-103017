a4aefcd5217df6ad2f4aa04bdb9e1b2658d5df19
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 21:15:50 +0000

    automatically backed up by learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -7,14 +7,15 @@ class City < ActiveRecord::Base
     #finds listing within that range
     #outputs those listing
     binding.pry
-    openings = Hash.new(0)
+    all_listings = Hash.new(0)
+    listings = Hash.new(0)
     self.listings.each do |listing|
       listing.reservations.each do |reservation|
         #binding.pry
         checkin = reservation.checkin
         checkout = reservation.checkout
-
-        #binding.pry
+        listings[listing] += 1
+        binding.pry
         if [datein.to_date, checkin].max > [dateout.to_date, checkout].min
           openings[listing] += 1
         end
